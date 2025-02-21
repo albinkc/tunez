9c3a5f8e913e468070b0e585bf7264b30588342a
Albin <8876252+albinkc@users.noreply.github.com>
2025-02-21 09:01:24 +0530

    calculations

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -56,4 +56,14 @@ defmodule Tunez.Music.Artist do
       sort year_released: :desc
     end
   end
+
+  calculations do
+    calculate :years_ago, :integer, expr(2025 - year_released)
+    calculate :album_count, :integer, expr(count(albums))
+    calculate :latest_album_year_released, :integer, expr(first(albums, field: :year_released))
+    calculate :cover_image_url, :string, expr(first(albums, field: :cover_image_url))
+  end
+
+  aggregates do
+  end
 end
